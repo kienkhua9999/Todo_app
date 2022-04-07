97543280f801a702f96c7743ed6e912010d3208b
kienkhua9999 <80381970+kienkhua9999@users.noreply.github.com>
2022-04-07 10:08:08 +0700

    first commit

---
+++ api/models/Todo.js
@@ -0,0 +1,53 @@
+/**
+ * Todo.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    name: {
+      required: true,
+      allowNull: false,
+      type: 'string',
+      maxLength: 200
+    },
+    dateStart: {
+      required: true,
+      type: 'string',
+      maxLength: 200,
+      allowNull: false
+    },
+    deadline: {
+      required: true,
+      type: 'string',
+      maxLength: 200,
+      allowNull: false
+    },
+    status: {
+      defaultsTo: false,
+      type: 'boolean'
+    },
+    userID: {
+      model: 'user'
+    }
+  },
+
+};
+
---
+++ api/policies/checkUserandEmail.js
@@ -0,0 +1,40 @@
+/**
+ * checkUserandEmail
+ *
+ * @module      :: Policy
+ * @description :: TODO: You might write a short summary of how this policy works and what it represents here.
+ * @help        :: http://sailsjs.org/#!/documentation/concepts/Policies
+ */
+module.exports = function(req, res, next) {
+
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then(user => {
+    if (user) {
+      res.status(400).send({
+        message: "Failed! Username is already in use!"
+      });
+      return;
+    }
+
+    // Email
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(user => {
+      if (user) {
+        res.status(400).send({
+          message: "Failed! Email is already in use!"
+        });
+        return;
+      }
+
+      next();
+    });
+  });
+          
+
+};
